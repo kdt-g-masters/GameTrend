2c2c5d2eaf728612653814a0d3787a7f1c2b204a
saeha <togk1996@gmail.com>
2022-03-24 04:52:35 +0900

    update index.jsp create game pkg change dir

--- gametrend/src/main/java/com/gmasters/gametrend/game/GameController.java
+++ gametrend/src/main/java/com/gmasters/gametrend/game/GameController.java
@@ -1,5 +1,36 @@
 package com.gmasters.gametrend.game;
 
-public class GameController {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GameController {
+	@Autowired
+	@Qualifier("gameservice")
+	GameService service;
+	
+	@RequestMapping("/list")
+	public ModelAndView gamelist(int page) {
+		System.out.println(page);
+		ModelAndView mv = new ModelAndView();
+		List<GameDTO> list = service.gamelist(page);
+		mv.addObject("paginglist",list);
+		mv.addObject("name", "페이징처리리스트");
+		mv.setViewName("index");
+		return mv;
+	}
+	
+	//@RequestMapping(value="/{platform}")
+	
+	
 }
